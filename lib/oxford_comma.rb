fb5250b20b07ae54cc49b5f7b885eb2322d6cea3
github username <github email address>
2019-10-21 14:22:10 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
+require "pry"
 def oxford_comma(array)
-
+  # binding.pry
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.length > 2
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ")
 end
